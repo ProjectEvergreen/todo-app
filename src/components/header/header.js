1effdd117c64a808e6526afd5f0be6a77d52281d
Owen Buckley <owen@thegreenhouse.io>
2018-05-27 16:06:52 -0400

    change project title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,7 +17,7 @@ class Header extends HTMLElement {
       
       <header>        
 
-        <h2 class="header-text">Project Evergreen: Todo MVC Example</h2>
+        <h2 class="header-text">Project Evergreen: Todo App Example</h2>
 
         <a href="https://github.com/ProjectEvergreen" target="_blank" rel="noopener">github</a> |
         <a href="https://twitter.com/PrjEvergreen" target="_blank" rel="noopener">twitter</a> 
