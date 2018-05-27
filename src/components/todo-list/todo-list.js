39ace1fed0399ea02f41c0e4b5542d019718ec3e
Owen Buckley <owen@thegreenhouse.io>
2018-05-27 10:49:06 -0400

    refactored event handling

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,4 +1,4 @@
-import { html, render } from 'lit-html';
+import { html, render } from 'lit-html/lib/lit-extended';
 import ValidationService from '../../services/validation';
 import css from './todo-list.css';
 
@@ -7,18 +7,15 @@ class TodoList extends HTMLElement {
   constructor() {
     super();
 
-    // TODO document true private properties
     this.todos = [];
     this.root = this.attachShadow({ mode: 'closed' });
 
     render(this.template(), this.root);
-    
-    // setup "static" event handlers after render
-    this.root.getElementById('add-todo').addEventListener('click', this.addTodo.bind(this));
   }
 
   addTodo() {
-    const newTodoValue = this.root.getElementById('todo-input').value;
+    const inputElement = this.root.getElementById('todo-input');
+    const newTodoValue = inputElement.value;
 
     if (ValidationService.isValidateTextInput(newTodoValue)) {
       const now = new Date().getTime();
@@ -30,8 +27,8 @@ class TodoList extends HTMLElement {
         created: now
       });
 
-      this.root.getElementById('todo-input').value = '';
-      this.onStateUpdated();
+      inputElement.value = '';
+      render(this.template(), this.root);
     } else {
       console.warn('invalid input, please try again'); // eslint-disable-line
     }
@@ -42,7 +39,7 @@ class TodoList extends HTMLElement {
       return todo.id !== parseInt(todoId, 10);
     });
 
-    this.onStateUpdated();
+    render(this.template(), this.root);
   }
 
   completeTodo(todoId) {
@@ -52,41 +49,21 @@ class TodoList extends HTMLElement {
       return todo;
     });
 
-    this.onStateUpdated();
-  }
-
-  onStateUpdated() {
-    // in this case, render our list first before attaching event handlers
-    // TODO considerations for performance, memory leakage?  Maybe something lit-html can help with (managing dynamic events)?
     render(this.template(), this.root);
-
-    const deleteButtons = this.root.querySelectorAll('.delete-todo');
-    const completeButtons = this.root.querySelectorAll('.complete-todo');
-
-    deleteButtons.forEach((button) => {
-      button.onclick = () => {
-        this.deleteTodo(button.getAttribute('todoid'));
-      };
-    });
-
-    completeButtons.forEach((button) => {
-      button.onchange = () => {
-        this.completeTodo(button.getAttribute('todoid'));
-      };
-    });
   }
 
+  // sync checkbox `checked` to value of `todo.completed`
   renderTodoListItems() {
     return this.todos.map((todo) => {
-      const isCompletedStatus = todo.completed ? '✅' : '⛔';
- 
+      const completionStatus = todo.completed ? '✅' : '⛔';
+
       return html`
         <li>
           ${todo.task}
 
-          <input class="complete-todo" type="checkbox" todoId="${todo.id}"/><span>${isCompletedStatus}</span>
+          <input class="complete-todo" type="checkbox" onchange=${() => { this.completeTodo(todo.id); }} /><span>${completionStatus}</span>
               
-          <span class="delete-todo" todoId="${todo.id}">X</span>
+          <span class="delete-todo" onclick=${() => { this.deleteTodo(todo.id); }}>X</span>
         </li>
       `;
 
@@ -103,7 +80,7 @@ class TodoList extends HTMLElement {
         <h3>My Todo List 📝</h3>
 
         <input id="todo-input" type="text" placeholder="Food Shopping" required/>
-        <button id="add-todo">+ Add</button>
+        <button id="add-todo" onclick=${ this.addTodo.bind(this) }>+ Add</button>
 
         <ol>
           ${ this.renderTodoListItems() }
